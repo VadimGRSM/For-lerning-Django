0e83072bf73b1608b83d910757f0c1a19549a7b6
VadimGRSM <vadimgrsm8923@gmail.com>
2024-04-12 14:54:33 +0300

    new html pages

--- vadimDJ/vadimDJ/urls.py
+++ vadimDJ/vadimDJ/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='hub'),
-    path('about', views.about, name='about')
-]
+    path('admin/', admin.site.urls),
+    path('', include('main.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
